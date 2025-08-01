5a7f5158b64b4786018ba6afff25e2bb24817d17
[dyad] <clouddark1798@gmail.com>
2025-08-01 10:44:28 +0700

    [dyad] Menambahkan komentar JSDoc ke ekspor modul di index.js untuk meningkatkan dukungan IDE. - wrote 1 file(s)

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ import * as services from './src/exports/services.js';
 import * as utils from './src/exports/utils.js';
 
 export default TahuJS;
-export { createTahu, quickChat, createQuickAgent, tools, plugins, services, utils };
+export { 
+    createTahu, 
+    quickChat, 
+    createQuickAgent, 
+    /**
+     * @namespace tools
+     * @description Kumpulan alat bawaan TahuJS yang dapat digunakan oleh agen atau secara langsung.
+     */
+    tools, 
+    /**
+     * @namespace plugins
+     * @description Kumpulan plugin TahuJS yang dapat memperluas fungsionalitas.
+     */
+    plugins, 
+    /**
+     * @namespace services
+     * @description Layanan inti TahuJS seperti SearchService dan MapService.
+     */
+    services, 
+    /**
+     * @namespace utils
+     * @description Utilitas umum TahuJS seperti ConfigValidator.
+     */
+    utils 
+};
